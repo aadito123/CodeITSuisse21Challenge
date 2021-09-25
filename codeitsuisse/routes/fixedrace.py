3f95b2b5fec2a78b2a3834ebe6541719e99b2ce6
aadito123 <63646058+aadito123@users.noreply.github.com>
2021-09-25 23:11:47 +0800

    dasd

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -1,6 +1,6 @@
 import logging
 import json
-
+import random
 from flask import request, jsonify
 
 from codeitsuisse import app
@@ -13,6 +13,10 @@ def evaluateFixedRace():
     data = request.data
     logging.info("data sent for evaluation {}".format(data))
     
-    result = request.data
+    result = request.data.decode('ASCII')
+    result = result.split(',')
+    random.shuffle(result)
+    result = ''.join(result).encode('ASCII')
+
     logging.info("My result :{}".format(result))
     return result
